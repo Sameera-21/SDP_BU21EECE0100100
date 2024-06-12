81c8e9340c724bbe9407f87d94258bbabd7db1aa
Sameera-21 <shaiksameera2003@gmail.com>
2024-06-12 10:34:16 +0530

    Update Digital output.c

--- Embedded C/Digital output.c
+++ Embedded C/Digital output.c
@@ -1,5 +1,7 @@
-#include <avr/io.h>
-#include <util/delay.h>
+//Create a simple program for an AVR microcontroller to blink an LED connected to pin PD3 in a continuous loop with equal on and off durations of 500 milliseconds each.
+
+#include <avr/io.h> // AVR I/O definitions
+#include <util/delay.h> // Delay functions
 
 // Initialize output pin
 void IO_Init() {
@@ -10,7 +12,7 @@ void IO_Init() {
 int main() {
     IO_Init(); // Call the function to initialize I/O
 
-    while (1) {
+    while (1) { // Infinite loop
         // Turn on the LED
         PORTD |= (1 << PD3);
         _delay_ms(500); // Wait for 500 milliseconds
