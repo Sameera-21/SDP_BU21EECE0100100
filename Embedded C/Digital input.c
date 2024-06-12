2731f865f487426966ad42ea9a18f5937a12bb5c
Sameera-21 <shaiksameera2003@gmail.com>
2024-06-12 10:33:30 +0530

    Update Digital input.c

--- Embedded C/Digital input.c
+++ Embedded C/Digital input.c
@@ -1,5 +1,7 @@
-#include <avr/io.h>
-#include <util/delay.h>
+//Configure an AVR microcontroller to control an LED (connected to pin PD3) based on the state of a push-button (connected to pin PD2), ensuring proper debouncing.
+
+#include <avr/io.h> // AVR I/O definitions
+#include <util/delay.h> // Delay functions
 
 void IO_Init() {
     // Set PD2 as input (push-button) and enable internal pull-up resistor
@@ -11,9 +13,9 @@ void IO_Init() {
 }
 
 int main() {
-    IO_Init();
+    IO_Init(); // Initialize I/O settings
 
-    while (1) {
+    while (1) { // Infinite loop
         // Read the state of the push-button
         if (PIND & (1 << PD2)) {
             // Button is not pressed (pull-up resistor makes it high)
@@ -23,6 +25,6 @@ int main() {
             PORTD |= (1 << PD3); // Turn on the LED
         }
 
-        _delay_ms(50); // Debounce delay
+        _delay_ms(50); // Debounce delay
     }
 }
