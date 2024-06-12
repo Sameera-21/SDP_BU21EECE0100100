c4c15351bf901c60d45a387a1bbca2e5fcf5c346
Sameera-21 <shaiksameera2003@gmail.com>
2024-06-12 10:29:18 +0530

    Update 8051 to control the output by switch.c

--- Embedded C/8051 to control the output by switch.c
+++ Embedded C/8051 to control the output by switch.c
@@ -1,40 +1,48 @@
-#include <reg51.h>
-sbit sw1 = P2^1;
-sbit sw2 = P2^2;
-void delay(unsigned int);
+//Program an 8051 microcontroller to control the output on port P1 based on the state of two input switches connected to port P2,
+demonstrating different output patterns for different switch combinations.
+#include <reg51.h> // Include header file for 8051 microcontroller (assuming this file contains necessary definitions for the microcontroller being used)
+
+sbit sw1 = P2^1; // Define switch 1 connected to pin P2.1
+sbit sw2 = P2^2; // Define switch 2 connected to pin P2.2
+
+void delay(unsigned int); // Function prototype for delay function
+
 void main(void)
 {
-	sw1 = 0;
-	sw2 = 0;
-	while(1)
+	sw1 = 0; // Initialize switch 1
+	sw2 = 0; // Initialize switch 2
+	
+	while(1) // Infinite loop for continuous operation
 	{
-		if(sw1==0 && sw2==0)
+		if(sw1==0 && sw2==0) // If both switches are off
 		{
-			P1=0x00;
+			P1=0x00; // Set all bits of port P1 to 0
 		}
-			else if (sw1==0 && sw2==1)
-			{
-				P1=0xAA;
-			}
-			else if (sw1==1 && sw2==0)	
-			{
-				P1=0x55;
-			}
-			else if (sw1==1 && sw2==1)	
-			{
-			//while(1)   // repeat forever
-				//{
-					P1=0x55; // toggle all bits of P1
-					delay(5000); // calling delay
-					P1=0xAA; // toggle all bits of P1
-					delay(5000); // calling delay
-				//}
-			}
+		else if (sw1==0 && sw2==1) // If switch 1 is off and switch 2 is on
+		{
+			P1=0xAA; // Set alternating bits of port P1 to 1
+		}
+		else if (sw1==1 && sw2==0)	// If switch 1 is on and switch 2 is off
+		{
+			P1=0x55; // Set alternating bits of port P1 to 0
+		}
+		else if (sw1==1 && sw2==1)	// If both switches are on
+		{
+			//while(1)   // repeat forever - (commented out, might be intended for continuous toggling)
+			//{
+				P1=0x55; // Set alternating bits of port P1 to 0
+				delay(5000); // Delay for 5000 iterations
+				P1=0xAA; // Set alternating bits of port P1 to 1
+				delay(5000); // Delay for 5000 iterations
+			//}
 		}
 	}
-void delay(unsigned int time)
+}
+
+void delay(unsigned int time) // Delay function definition
 {
 	unsigned int i, j;
-	for (i=0;i<time;i++)
-		for (j=0;j<1275;j++);
+	for (i=0;i<time;i++) // Outer loop for time delay
+		for (j=0;j<1275;j++); // Inner loop for fine-tuning the delay
 }
+
