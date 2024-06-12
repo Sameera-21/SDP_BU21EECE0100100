db72a8bd32495fdd3ad7e615ffdb03f0ab9adae0
Sameera-21 <shaiksameera2003@gmail.com>
2024-06-12 10:36:51 +0530

    Update Serial communication.c

--- Embedded C/Serial communication.c
+++ Embedded C/Serial communication.c
@@ -1,14 +1,19 @@
-#include <reg51.h>
+//The objective of this code is to configure an 8051 microcontroller to transmit the 
+character 'B' over serial communication using a baud rate of 4800 bits per second.
+
+#include <reg51.h> // Include header file for 8051 microcontroller
+
 void main(void)
 {
-	TMOD = 0X20; //use Timer 1, mode 2
-	TH1 = 0XFA; //4800 baud rate
-	SCON = 0X50;
-	TR1 = 1; //start timer
+	TMOD = 0X20; // Use Timer 1, mode 2 for serial communication
+	TH1 = 0XFA; // Set TH1 to achieve 4800 baud rate
+	SCON = 0X50; // Set serial mode: 8-bit data, variable baud rate, enable receiver
+	TR1 = 1; // Start Timer 1 for serial communication
+	
 	while(1)
 	{
-		SBUF ='B';
-		while(TI==0);
-		TI=0;
+		SBUF ='B'; // Load 'B' character into serial buffer for transmission
+		while(TI==0); // Wait until the transmission is complete
+		TI=0; // Clear transmit interrupt flag for the next transmission
 	}
 }
