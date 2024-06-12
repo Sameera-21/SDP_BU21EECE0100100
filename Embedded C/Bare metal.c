234df52606198be77418947e043150559f30859c
Sameera-21 <shaiksameera2003@gmail.com>
2024-06-12 10:32:19 +0530

    Update Bare metal.c

--- Embedded C/Bare metal.c
+++ Embedded C/Bare metal.c
@@ -1,11 +1,12 @@
-#include <avr/io.h>
-#include <util/delay.h>
+//Blink an LED connected to pin PD6 of an AVR microcontroller in a continuous loop with a delay of 500 milliseconds between each toggle.
+#include <avr/io.h> // AVR I/O definitions
+#include <util/delay.h> // Delay functions
 
 int main() {
     // Set PD6 as output
     DDRD |= (1 << PD6);
 
-    while (1) {
+    while (1) { // Infinite loop
         // Toggle the LED
         PORTD ^= (1 << PD6);
         // Delay for a short time
